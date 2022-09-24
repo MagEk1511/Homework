957082ae8bf0dbc12493b2dd3cdadc4fb4f20057
MagEk1511 <magerramov_er_1021@1511.ru>
2022-09-24 13:07:42 +0300

    solved second task

---
+++ src/Task3.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input: ");
+        var i = in.nextInt();
+        System.out.println("Output: " + i / 3600 % 24 + ":" + ((i - i / 3600 * 3600) / 60 < 10 ? "0" : "") + (i - i / 3600 * 3600) / 60 + ":" + (i % 60 < 10 ? "0" : "") + i % 60 );
+    }
+}
