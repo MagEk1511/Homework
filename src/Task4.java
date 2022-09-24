36c958ed1a4e8f280525e59270a7f54c38b9f2f7
MagEk1511 <magerramov_er_1021@1511.ru>
2022-09-24 14:41:59 +0300

    solved fourth task. Input with dot

--- src/Task4.java
+++ src/Task4.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
 
 public class Task4 {
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String s = in.nextLine();
         var i = Float.parseFloat(s);
         System.out.println(i < -2 || (i > 3 && i < 6) || i > 9);
     }
+
 }
