9e4d563458aad50011304ab93e38b28ee8f632cc
MagEk1511 <magerramov_er_1021@1511.ru>
2022-10-06 17:31:16 +0300

    solved third task

--- src/Task3.java
+++ src/Task3.java
@@ -1,32 +1,40 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Task3{
-    public static class MyArrays{
-        public static int[] minToBegin(int[] array){
+public class Task3 {
+    public static class MyArrays {
+        public static int minToBegin(int[] array) {
             int min = array[0];
             int min_index = 0;
             int index = 0;
-            for(int i : array){
-                if(i < min){min = i; min_index = index;}
+            int equal_quant = 0;
+            for (int i : array) {
+                if (i < min) {
+                    min = i;
+                    min_index = index;
+                    equal_quant = 0;
+                }
+                if(min == i) ++equal_quant;
                 ++index;
             }
-            for(int i = min_index; i > 0; --i){
-                array[i] = array[i-1];
+            if(equal_quant == array.length) return -1;
+            for (int i = min_index; i > 0; --i) {
+                array[i] = array[i - 1];
             }
             array[0] = min;
-            return array;
+            return 0;
         }
     }
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int arr_len = in.nextInt();
         int[] array = new int[arr_len];
-        for(int i = 0; i < arr_len; ++i){
+        for (int i = 0; i < arr_len; ++i) {
             array[i] = in.nextInt();
         }
-        System.out.println(Arrays.toString(MyArrays.minToBegin(array)));
+        System.out.println(MyArrays.minToBegin(array));
+        System.out.println(Arrays.toString(array));
     }
 
 }
